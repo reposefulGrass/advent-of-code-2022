8b7d96ce81b3dbd279854d6b871832ec54ed45c1
Corey Wingo <reposefulGrass@gmail.com>
2022-12-01 00:53:31 -0600

    Day 1 Solved

---
+++ day-01/src/main.rs
@@ -0,0 +1,46 @@
+
+fn main() {
+    let input = include_str!("../input/input.txt");
+
+    part_a(input);
+    part_b(input);
+}
+
+fn part_a(input: &str) {
+    let elf_groups: Vec<&str> = input.split("\r\n\r\n").collect();
+    let mut most_calories = 0;
+
+    for group in elf_groups {
+        let mut calories: u32 = 0;
+        for calories_str in group.lines() {
+            calories += calories_str.parse::<u32>().unwrap();
+        }   
+        if calories > most_calories {
+            most_calories = calories;
+        }
+    }
+
+    println!("The most calories carried is {}.", most_calories);
+}
+
+fn part_b(input: &str) {
+    let elf_groups: Vec<&str> = input.split("\r\n\r\n").collect();
+    let mut elf_calories: Vec<u32> = vec![];
+
+    for group in elf_groups {
+        let mut calories: u32 = 0;
+        for calories_str in group.lines() {
+            calories += calories_str.parse::<u32>().unwrap();
+        }
+        elf_calories.push(calories);
+    }
+
+    elf_calories.sort();
+
+    let most_three_calories = 
+        elf_calories.pop().unwrap() + 
+        elf_calories.pop().unwrap() + 
+        elf_calories.pop().unwrap();
+
+    println!("The total calories of the top 3 elfs is {}.", most_three_calories);
+}
