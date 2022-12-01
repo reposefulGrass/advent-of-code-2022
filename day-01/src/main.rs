76b8d2454ca849c1b454b845c721a0a9f994244e
Corey Wingo <reposefulGrass@gmail.com>
2022-12-01 01:20:10 -0600

    Refactor

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -6,14 +6,6 @@ fn main() {
     part_b(input);
 }
 
-fn elf_calories(input: &str) -> Vec<u32> {
-    input.split("\r\n\r\n")
-        .map(|elf| elf.lines()
-            .map(|calories| calories.parse::<u32>().unwrap())
-            .sum())
-        .collect()
-}
-
 fn part_a(input: &str) {
     let mut elves = elf_calories(input);
     elves.sort();
@@ -29,6 +21,14 @@ fn part_b(input: &str) {
 
     let most_three_calories: u32 = elves[0..3].iter().sum();
     println!("The total calories of the top 3 elfs is {}.", most_three_calories);
+}
+
+fn elf_calories(input: &str) -> Vec<u32> {
+    input.split("\r\n\r\n")
+        .map(|elf| elf.lines()
+            .map(|calories| calories.parse::<u32>().unwrap())
+            .sum())
+        .collect()
 }
 
 #[test]
