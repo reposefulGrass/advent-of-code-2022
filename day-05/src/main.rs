ac1aebe67da54265029cd9b748717150210664d6
Corey Wingo <reposefulGrass@gmail.com>
2022-12-09 19:30:26 -0600

    Day 5 Solved

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,20 +1,68 @@
 
 mod supplies;
 
+use lazy_static::lazy_static;
+use regex::Regex;
 use supplies::*;
 
+lazy_static! {
+    static ref COMMAND_PATTERN: Regex = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+}
+
+
 fn main() {
-    let input = include_str!("../input/test.txt");
+    let input = include_str!("../input/input.txt");
 
     part_a(input);
+    part_b(input);
 }
 
 fn part_a(input: &str) {
-    let mut supplies = StackedSupplies::new(3);
+    let num_crates = determine_num_of_crates(input) as u32;
+    let mut supplies = StackedSupplies::new(num_crates);
+
     supplies.parse(grab_only_crates(input));
+
+    for line in grab_only_commands(input).lines() {
+        let (how_many, from, to) = parse_command(line);
+        supplies.move_supply_ver_1(how_many, from as usize, to as usize);
+    }
+
+    println!("[Part A] The crates at the top of the stacks are {}", supplies.top_of_piles());
+}
+
+fn part_b(input: &str) {
+    let num_crates = determine_num_of_crates(input) as u32;
+    let mut supplies = StackedSupplies::new(num_crates);
+
+    supplies.parse(grab_only_crates(input));
+
+    for line in grab_only_commands(input).lines() {
+        let (how_many, from, to) = parse_command(line);
+        supplies.move_supply_ver_2(how_many, from as usize, to as usize);
+    }
+
+    println!("[Part B] The crates at the top of the stacks are {}", supplies.top_of_piles());
+}
+
+fn determine_num_of_crates(input: &str) -> usize {
+    (input.lines().nth(0).unwrap().len() + 1) / 4
 }
 
 fn grab_only_crates(input: &str) -> &str {
     let index = input.rfind("]").unwrap();
     &input[0 .. index + 1]
+}
+
+fn grab_only_commands(input: &str) -> &str {
+    let index = input.rfind("\r\n\r\n").unwrap();
+    &input[index+4..]
+}
+
+fn parse_command(input: &str) -> (u32, u32, u32) {
+    let captures = COMMAND_PATTERN.captures(input).unwrap();
+    let how_many: u32 = captures.get(1).unwrap().as_str().parse().unwrap();
+    let from: u32 = captures.get(2).unwrap().as_str().parse().unwrap();
+    let to: u32 = captures.get(3).unwrap().as_str().parse().unwrap();
+    (how_many, from - 1, to - 1)
 }
--- day-05/src/supplies.rs
+++ day-05/src/supplies.rs
@@ -1,28 +1,96 @@
 
+#[derive(Debug)]
 pub struct StackedSupplies {
     num_piles: u32,
-    piles: Vec<Vec<u32>>,
+    piles: Vec<Vec<char>>,
 }
 
 impl StackedSupplies {
     pub fn new(num_piles: u32) -> Self {
         let mut supplies = StackedSupplies {
             num_piles,
-            piles: vec![],
+            piles: Vec::new(),
         };
 
         for _ in 0..num_piles {
-            supplies.piles.append(&mut Vec::new())
+            supplies.piles.push(Vec::new())
         }
 
         supplies
     }
 
+    ///     [D]    
+    /// [N] [C]    
+    /// [Z] [M] [P]
+    ///
+    /// Parses the above format into a list of vectors, each containing the characters within.
     pub fn parse(&mut self, input: &str) {
         for line in input.lines() {
-            for pile in 0 .. num_piles {
-                
+            for pile in 0 .. self.num_piles {
+                let selected_pile: &mut Vec<char> = self.piles.get_mut(pile as usize).unwrap();
+
+                let begin_of_crate = (pile * 4) as usize;
+                if let Some(supply_crate) = Self::parse_crate(&line[begin_of_crate..begin_of_crate+3]) {
+                    selected_pile.push(supply_crate);
+                }
             }
         }
+
+        for pile in 0 .. self.num_piles {
+            let selected_pile: &mut Vec<char> = self.piles.get_mut(pile as usize).unwrap();
+            selected_pile.reverse();
+        }
+    }
+
+    /// Takes a slice of a crate " [X] " and returns the character within: X.
+    /// If there is no crate "    ", then return None.
+    fn parse_crate(input: &str) -> Option<char> {
+        let character = input.trim().chars().nth(1)?;
+        if character.is_ascii_alphabetic() {
+            Some(character)
+        } else {
+            None
+        }
+    }
+
+    /// Move `how_many` crates in reverse order from `from` to `to`. 
+    pub fn move_supply_ver_1(&mut self, how_many: u32, from: usize, to: usize) {
+        let pile_from = self.piles.get_mut(from).unwrap();
+        let mut temp = Vec::new();
+        for _ in 0..how_many {
+            temp.push(pile_from.pop().unwrap());
+        }
+        temp.reverse();
+
+        let pile_to = self.piles.get_mut(to).unwrap();
+        for _ in 0..how_many {
+            pile_to.push(temp.pop().unwrap());
+        }
+    }
+
+    /// Move `how_many` crates in the same order from `from` to `to`.
+    pub fn move_supply_ver_2(&mut self, how_many: u32, from: usize, to: usize) {
+        let pile_from = self.piles.get_mut(from).unwrap();
+        let mut temp = Vec::new();
+        for _ in 0..how_many {
+            temp.push(pile_from.pop().unwrap());
+        }
+
+        let pile_to = self.piles.get_mut(to).unwrap();
+        for _ in 0..how_many {
+            pile_to.push(temp.pop().unwrap());
+        }
+    }
+
+    /// Grab the top of each pile of crates.
+    pub fn top_of_piles(&mut self) -> String {
+        let mut output = String::from("");
+
+        for pile in 0..self.num_piles {
+            let selected_pile: &mut Vec<char> = self.piles.get_mut(pile as usize).unwrap();
+            output.push_str(&selected_pile.pop().unwrap().to_string());
+        }
+
+        output
     }
 }
